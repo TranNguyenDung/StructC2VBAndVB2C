388882dd4c4cae9149e00cf54b3f314f93d412c2
Tran Nguyen Dung <tnguyendung.x1@gmail.com>
2022-02-17 15:08:18 +0700

    Start commit

---
+++ StructC2VBAndVB2C/Form1.Designer.vb
@@ -0,0 +1,158 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CODE_VB = New System.Windows.Forms.TextBox()
+        Me.CODE_C = New System.Windows.Forms.TextBox()
+        Me.Name = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.VB2C = New System.Windows.Forms.Button()
+        Me.C2VB = New System.Windows.Forms.Button()
+        Me.btnRedim = New System.Windows.Forms.Button()
+        Me.btnSetData = New System.Windows.Forms.Button()
+        Me.btnGetData = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'CODE_VB
+        '
+        Me.CODE_VB.Location = New System.Drawing.Point(24, 115)
+        Me.CODE_VB.Multiline = True
+        Me.CODE_VB.Name = "CODE_VB"
+        Me.CODE_VB.Size = New System.Drawing.Size(265, 669)
+        Me.CODE_VB.TabIndex = 0
+        '
+        'CODE_C
+        '
+        Me.CODE_C.Location = New System.Drawing.Point(424, 115)
+        Me.CODE_C.Multiline = True
+        Me.CODE_C.Name = "CODE_C"
+        Me.CODE_C.Size = New System.Drawing.Size(270, 669)
+        Me.CODE_C.TabIndex = 1
+        '
+        'Name
+        '
+        Me.Name.Font = New System.Drawing.Font("Microsoft Sans Serif", 26.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name.Location = New System.Drawing.Point(160, 20)
+        Me.Name.Name = "Name"
+        Me.Name.Size = New System.Drawing.Size(414, 48)
+        Me.Name.TabIndex = 2
+        Me.Name.Text = "Convert Struct C And VB"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(511, 78)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 25)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "CODE C"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(103, 78)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(106, 25)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "CODE VB"
+        '
+        'VB2C
+        '
+        Me.VB2C.Location = New System.Drawing.Point(310, 115)
+        Me.VB2C.Name = "VB2C"
+        Me.VB2C.Size = New System.Drawing.Size(86, 61)
+        Me.VB2C.TabIndex = 5
+        Me.VB2C.Text = ">>"
+        Me.VB2C.UseVisualStyleBackColor = True
+        '
+        'C2VB
+        '
+        Me.C2VB.Location = New System.Drawing.Point(310, 182)
+        Me.C2VB.Name = "C2VB"
+        Me.C2VB.Size = New System.Drawing.Size(86, 58)
+        Me.C2VB.TabIndex = 6
+        Me.C2VB.Text = "<<Define"
+        Me.C2VB.UseVisualStyleBackColor = True
+        '
+        'btnRedim
+        '
+        Me.btnRedim.Location = New System.Drawing.Point(310, 246)
+        Me.btnRedim.Name = "btnRedim"
+        Me.btnRedim.Size = New System.Drawing.Size(86, 58)
+        Me.btnRedim.TabIndex = 7
+        Me.btnRedim.Text = "<<Redim"
+        Me.btnRedim.UseVisualStyleBackColor = True
+        '
+        'btnSetData
+        '
+        Me.btnSetData.Location = New System.Drawing.Point(310, 310)
+        Me.btnSetData.Name = "btnSetData"
+        Me.btnSetData.Size = New System.Drawing.Size(86, 59)
+        Me.btnSetData.TabIndex = 8
+        Me.btnSetData.Text = "<<SetData"
+        Me.btnSetData.UseVisualStyleBackColor = True
+        '
+        'btnGetData
+        '
+        Me.btnGetData.Location = New System.Drawing.Point(310, 375)
+        Me.btnGetData.Name = "btnGetData"
+        Me.btnGetData.Size = New System.Drawing.Size(86, 59)
+        Me.btnGetData.TabIndex = 9
+        Me.btnGetData.Text = "<<GetData"
+        Me.btnGetData.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(724, 796)
+        Me.Controls.Add(Me.btnGetData)
+        Me.Controls.Add(Me.btnSetData)
+        Me.Controls.Add(Me.btnRedim)
+        Me.Controls.Add(Me.C2VB)
+        Me.Controls.Add(Me.VB2C)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Name)
+        Me.Controls.Add(Me.CODE_C)
+        Me.Controls.Add(Me.CODE_VB)
+        Me.Text = "Tool Convert VB2C Ver 1.2"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CODE_VB As TextBox
+    Friend WithEvents CODE_C As TextBox
+    Friend WithEvents Name As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents VB2C As Button
+    Friend WithEvents C2VB As Button
+    Friend WithEvents btnRedim As Button
+    Friend WithEvents btnSetData As Button
+    Friend WithEvents btnGetData As Button
+End Class
---
+++ StructC2VBAndVB2C/Form1.vb
@@ -0,0 +1,256 @@
+﻿
+Public Class Form1
+    Private Sub VB2C_Click(sender As Object, e As EventArgs) Handles VB2C.Click
+        'Dim ledPwm As Byte
+        'Dim ledPwm2 As Byte
+        'Dim aircloneId As Byte
+        'Dim volGuide As Byte
+        'Dim setteiId() As Byte
+        'Dim tokuExtend As Byte
+        'Dim txLimitOff As Byte
+
+        'unsigned Char lo_pwr;
+        'unsigned Char group;
+        'unsigned Char code;
+        'unsigned Char comp;
+        'unsigned Char vox;
+        'unsigned Char mic_gain;
+        'unsigned Char guide_vol;
+        'unsigned Char rec;
+        Dim strDmy As String
+        strDmy = CODE_VB.Text
+        strDmy = strDmy.Replace("Dim", "unsigned char")
+        strDmy = strDmy.Replace(" As Byte", ";")
+
+        CODE_C.Text = strDmy
+        'Debug.WriteLine(CODE_C.Text)
+    End Sub
+
+    Private Sub C2VB_Click(sender As Object, e As EventArgs) Handles C2VB.Click
+        Dim strDmy As String
+        strDmy = CODE_C.Text
+        Dim strData() As String = strDmy.Split(vbCrLf)
+        strDmy = ""
+        For i = 0 To strData.Length - 1
+            strData(i) = strData(i).Replace(vbLf, "").Replace(vbTab, "")
+            If (strData(i) <> "") And (strData(i).StartsWith("//") = False) Then
+                ' Erase char(//) to enter
+                strData(i) = strData(i).Substring(0, strData(i).IndexOf(";"))
+                Debug.WriteLine(strData(i))
+                ' Process
+                If strData(i).Contains("struct") = False Then
+                    If (strData(i).Contains("[") = True) And (strData(i).Contains("]") = True) And (strData(i).Split("[").Length = 2) Then
+                        'Get Number Init
+                        Dim StrLen As String = strData(i).Split("[")(1).Trim
+                        StrLen = StrLen.Split("]")(0).Trim
+
+                        strData(i) = strData(i).Split("[")(0).Replace("unsigned char ", "").Trim
+                        strData(i) = "Dim " & strData(i).Trim & "() as byte"
+
+                    ElseIf (strData(i).Contains("[") = False) And (strData(i).Contains("]") = False) Then
+                        strData(i) = strData(i).Replace("unsigned char ", "").Trim
+                        strData(i) = "Dim " & strData(i).Trim & " as byte"
+                    Else
+                        strData(i) = strData(i) & "<<< Not fix"
+                    End If
+                Else
+                    If strData(i).Contains("[") = False Then
+                        Dim tmp() As String = strData(i).Split(" ")
+                        strData(i) = "Dim " & tmp(2).Trim & " as " & tmp(1).ToUpper
+                    Else
+                        If strData(i).Split("[").Length = 2 Then
+                            Dim tmp() As String = strData(i).Split(" ")
+                            tmp(2) = tmp(2).Substring(0, tmp(2).IndexOf("["))
+                            strData(i) = "Dim " & tmp(2).Trim & "() as " & tmp(1).ToUpper
+                        Else
+                            strData(i) = strData(i) & "<<< Not fix"
+                        End If
+                    End If
+                End If
+                    ' Erase space
+                    strDmy = strDmy & strData(i).Trim & vbCrLf
+            End If
+
+        Next
+        CODE_VB.Text = strDmy
+    End Sub
+
+    Private Sub btnRedim_Click(sender As Object, e As EventArgs) Handles btnRedim.Click
+        Dim strDmy As String
+        strDmy = CODE_C.Text
+        Dim strData() As String = strDmy.Split(vbCrLf)
+        strDmy = ""
+        For i = 0 To strData.Length - 1
+            strData(i) = strData(i).Replace(vbLf, "").Replace(vbTab, "")
+            If (strData(i) <> "") And (strData(i).StartsWith("//") = False) Then
+                ' Erase char(//) to enter
+                strData(i) = strData(i).Substring(0, strData(i).IndexOf(";"))
+                Debug.WriteLine(strData(i))
+                ' Process
+                If strData(i).Contains("struct") = False Then
+                    If (strData(i).Contains("[") = True) And (strData(i).Contains("]") = True) And (strData(i).Split("[").Length = 2) Then
+                        'Get Number Init
+                        Dim StrLen As String = strData(i).Split("[")(1).Trim
+                        StrLen = StrLen.Split("]")(0).Trim
+
+                        strData(i) = strData(i).Split("[")(0).Replace("unsigned char ", "").Trim
+                        strData(i) = "Redim " & strData(i) & "(0 to " & (CInt(StrLen) - 1).ToString & ")"
+                        ' Erase space
+                        strDmy = strDmy & strData(i).Trim & vbCrLf
+                    End If
+                Else
+                    If strData(i).Contains("[") = False Then
+                        Dim tmp() As String = strData(i).Split(" ")
+                        strData(i) = tmp(2).Trim & ".InitST()"
+                    Else
+                        If strData(i).Split("[").Length = 2 Then
+                            'Get name
+                            Dim name As String = strData(i).Split(" ")(2)
+                            name = name.Substring(0, name.IndexOf("["))
+                            'Get len
+                            Dim strlen As String = strData(i).Split("[")(1)
+                            strlen = strlen.Split("]")(0)
+                            strlen = (CInt(strlen) - 1).ToString
+
+                            strData(i) = "ReDim " & name & "(0 to " & strlen & ")" & vbCrLf
+                            strData(i) = strData(i) & "For i = 0 To " & name & ".Count - 1" & vbCrLf
+                            strData(i) = strData(i) & "     " & name & "(i).InitST()" & vbCrLf
+                            strData(i) = strData(i) & "Next" & vbCrLf
+                        Else
+                            strData(i) = strData(i) & "<<< Not fix"
+                        End If
+                    End If
+                    ' Erase space
+                    strDmy = strDmy & strData(i).Trim & vbCrLf
+                End If
+            End If
+        Next
+        CODE_VB.Text = strDmy
+    End Sub
+
+    Private Sub SetData_Click(sender As Object, e As EventArgs) Handles btnSetData.Click
+        Dim strDmy As String
+        strDmy = CODE_C.Text
+        Dim strData() As String = strDmy.Split(vbCrLf)
+        strDmy = ""
+        For i = 0 To strData.Length - 1
+            strData(i) = strData(i).Replace(vbLf, "").Replace(vbTab, "")
+            If (strData(i) <> "") And (strData(i).StartsWith("//") = False) Then
+                ' Erase char(//) to enter
+                strData(i) = strData(i).Substring(0, strData(i).IndexOf(";"))
+                Debug.WriteLine(strData(i))
+
+                ' Process
+                If strData(i).Contains("struct") = False Then
+                    If (strData(i).Contains("[") = True) And (strData(i).Contains("]") = True) And (strData(i).Split("[").Length = 2) Then
+                        strData(i) = strData(i).Split("[")(0).Replace("unsigned char ", "").Trim
+                        'strData(i) = "SetData(" & strData(i) & ", Buff, intPos, " & strData(i) & ".length)"
+                        strData(i) = "SetData(" & strData(i) & ", Buff, intPos)"
+                    ElseIf (strData(i).Contains("[") = False) And (strData(i).Contains("]") = False) Then
+                        strData(i) = strData(i).Replace("unsigned char ", "").Trim
+                        strData(i) = "SetData(" & strData(i) & ", Buff, intPos)"
+                    Else
+                        strData(i) = strData(i) & "<<< Not fix"
+                    End If
+                Else
+                    If strData(i).Contains("[") = False Then
+                        Dim tmp() As String = strData(i).Split(" ")
+                        strData(i) = tmp(2).Trim & ".SetST(Buff, intPos)"
+                    Else
+                        If strData(i).Split("[").Length = 2 Then
+                            'Get name
+                            Dim name As String = strData(i).Split(" ")(2)
+                            name = name.Substring(0, name.IndexOf("["))
+                            strData(i) = ""
+                            strData(i) = strData(i) & "For i = 0 To " & name & ".Count - 1" & vbCrLf
+                            strData(i) = strData(i) & "     " & name.Trim & "(i).SetST(Buff, intPos)" & vbCrLf
+                            strData(i) = strData(i) & "Next" & vbCrLf
+                        Else
+                            strData(i) = strData(i) & "<<< Not fix"
+                        End If
+                    End If
+                End If
+
+                ' Erase space
+                strDmy = strDmy & strData(i).Trim & vbCrLf
+            End If
+        Next
+        CODE_VB.Text = strDmy
+    End Sub
+
+    Private Sub GetData_Click(sender As Object, e As EventArgs) Handles btnGetData.Click
+        Dim strDmy As String
+        strDmy = CODE_C.Text
+        Dim strData() As String = strDmy.Split(vbCrLf)
+        strDmy = ""
+        For i = 0 To strData.Length - 1
+            strData(i) = strData(i).Replace(vbLf, "").Replace(vbTab, "")
+            If (strData(i) <> "") And (strData(i).StartsWith("//") = False) Then
+                ' Erase char(//) to enter
+                strData(i) = strData(i).Substring(0, strData(i).IndexOf(";"))
+                Debug.WriteLine(strData(i))
+
+                ' Process
+                If strData(i).Contains("struct") = False Then
+                    If (strData(i).Contains("[") = True) And (strData(i).Contains("]") = True) And (strData(i).Split("[").Length = 2) Then
+                        strData(i) = strData(i).Split("[")(0).Replace("unsigned char ", "").Trim
+                        'strData(i) = "GetData(" & strData(i) & ", Buff, intPos, " & strData(i) & ".length)"
+                        strData(i) = "GetData(" & strData(i) & ", Buff, intPos)"
+                    ElseIf (strData(i).Contains("[") = False) And (strData(i).Contains("]") = False) Then
+                        strData(i) = strData(i).Replace("unsigned char ", "").Trim
+                        strData(i) = "GetData(" & strData(i) & ", Buff, intPos)"
+                    Else
+                        strData(i) = strData(i) & "<<< Not fix"
+                    End If
+                Else
+                    If strData(i).Contains("[") = False Then
+                        Dim tmp() As String = strData(i).Split(" ")
+                        strData(i) = tmp(2).Trim & ".GetST(Buff, intPos)"
+                    Else
+                        If strData(i).Split("[").Length = 2 Then
+                            'Get name
+                            Dim name As String = strData(i).Split(" ")(2)
+                            name = name.Substring(0, name.IndexOf("["))
+                            strData(i) = ""
+                            strData(i) = strData(i) & "For i = 0 To " & name & ".Count - 1" & vbCrLf
+                            strData(i) = strData(i) & "     " & name.Trim & "(i).GetST(Buff, intPos)" & vbCrLf
+                            strData(i) = strData(i) & "Next" & vbCrLf
+                        Else
+                            strData(i) = strData(i) & "<<< Not fix"
+                        End If
+                    End If
+                End If
+
+                ' Erase space
+                strDmy = strDmy & strData(i).Trim & vbCrLf
+            End If
+        Next
+        CODE_VB.Text = strDmy
+    End Sub
+End Class
+
+#If 0 Then
+
+		unsigned char led_pwm;			
+		unsigned char led_pwm2;	
+		unsigned char airclone_id;	
+		unsigned char vol_guide;	
+		unsigned char settei_id[2];			
+		unsigned char toku_extend;			
+		unsigned char tx_limit_off;			
+					
+		unsigned char alarm_on;			
+		unsigned char alarm_time[3];
+		unsigned char temp_alrm_on;
+		unsigned char temp_th;
+		unsigned char am_th;
+		unsigned char fm_th;
+
+
+redim 
+setteiid(0 to 1)       
+redim 
+alarmtime(0 to 2)       
+    
+
+#End If
